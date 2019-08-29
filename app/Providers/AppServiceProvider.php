7f98123e655cfbe202f98b2fd05e46237aff4d13
ashmdev <abdelkarinhm@gmail.com>
2019-08-29 18:49:39 -0400

    Integrando Laravel 5 IDE Helper Generator Paquete que proporciona autompletado para los IDE php artisan ide-helper:generate - phpDoc generation for Laravel Facades php artisan ide-helper:models - phpDocs for models php artisan ide-helper:meta - PhpStorm Meta file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
